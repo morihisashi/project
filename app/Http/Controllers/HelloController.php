96085fe883bb6151d5c59c92ae8094c8114ee085
morihisashi <hisa.1995.322@outlook.com>
2022-05-03 11:06:39 +0900

    Laravel20220503

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -12,10 +12,7 @@ function tag($tag, $txt){
 class HelloController extends Controller
 {
     public function index(){
-        $data = [
-            'msg'=>''
-        ];
-        return view('hello.index',$data);
+        return view('hello.index',['msg'=>'']);
     }
 
     public function post(Request $request){
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <title>Hello/Index</title>
+    <title>Blade/Index</title>
     <style>
         body {font-size:16pt; color:#999; }
         h1 {font-size:50pt; text-align:right; color:#f6f6f6; 
